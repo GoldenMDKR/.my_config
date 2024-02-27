3c61c63bc51b941913b675839a192a0e85ecd3f6
mederick <mederick.rouille@gmail.com>
2024-02-27 16:15:43 +0100

    finished install scripts with minor change to make it work

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # install yay first
-pacman -S yay
+sudo pacman -S --needed yay
 
 # install all package trough yay
-yay -S - < install_files/pacman_packages_list.txt
+yay -S --needed - < install_files/pacman_packages_list.txt
 
 # create symlinks for dotfiles
 bash install_files/symlink_list.txt
@@ -16,6 +16,19 @@ bash install_files/symlink_list.txt
 git clone --depth=1 https://github.com/adi1090x/rofi.git
 cd rofi
 chmod +x setup.sh
-./setup.sh
+bash ./setup.sh
 cd ..
 rm -rf rofi
+
+
+cp -rf wallpapers $HOME/Pictures/
+
+
+# setup lockscreen wallpaper 
+betterlockscreen -u $HOME/Picture/wallpapers/megumin_lock.png
+
+# setup wallpaper 
+nitrogen --set-scaled $HOME/Pictures/wallpapers/tanya.png
+
+# throw an error and open nitrogen just save manualy is needed
+nitrogen --save 
